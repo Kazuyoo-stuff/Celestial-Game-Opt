759a161ef03ec131c2ccb96be26d22f31f12f024
Kazuyoo_12 <dikinurrohmat42@gmail.com>
2025-03-22 11:39:36 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,43 +1,63 @@
 #!/system/bin/sh
+# stops execution if the user running it is not UID 2000.
+[ "$(id -u)" -ne 2000 ] && echo "[WARN] No shell permissions." && exit 1
+
 # Load dependencies
 source /storage/emulated/0/Kazu/META-INF/com/google/android/update-binary &>/dev/null || { echo "[ ! ] Error: Unable to load update-binary script."; exit 1; }
 
-# array / variabel
+# Set logcat buffer size
+LOGCAT_BUFFER=$(getprop dalvik.vm.heapgrowthlimit)
+if [ -n "$LOGCAT_BUFFER" ]; then
+    logcat -G "$LOGCAT_BUFFER"
+else
+    logcat -G 256K
+fi
+
+# Definition of variables
+PROPFILE=true
+POSTFSDATA=true
+LATESTARTSERVICE=true
 NAME="Celestial-Game-Opt | Kzyo"
-VERSION="1.0"
+VERSION="1.3"
 DATE="Wed 8 Jan 2025"
 
-sleep 0.2
+# Function to print with a clearer format
+print_line() {
+    ui_print "***************************************"
+}
+
 ui_print ""
 ui_print "░█▀▀█ ── ░█▀▀█ ─█▀▀█ ░█▀▄▀█ ░█▀▀▀ ── ░█▀▀▀█ ░█▀▀█ ▀▀█▀▀ 
 ░█─── ▀▀ ░█─▄▄ ░█▄▄█ ░█░█░█ ░█▀▀▀ ▀▀ ░█──░█ ░█▄▄█ ─░█── 
 ░█▄▄█ ── ░█▄▄█ ░█─░█ ░█──░█ ░█▄▄▄ ── ░█▄▄▄█ ░█─── ─░█──"
 ui_print ""
 sleep 0.5
-ui_print " tweaks & improvements to the Game."
+ui_print " Tweaks & improvements for better gaming performance."
 ui_print ""
-sleep 0.2
-ui_print "***************************************"
+print_line
 ui_print "- Name            : ${NAME}"
 sleep 0.2
 ui_print "- Version         : ${VERSION}"
 sleep 0.2
-ui_print "- Android Version : ${ANDROIDVERSION}"
+ui_print "- Android Version : ${ANDROIDVERSION:-Unknown}"
 sleep 0.2
 ui_print "- Build Date      : ${DATE}"
+print_line
 sleep 0.2
-ui_print "***************************************"
-ui_print "- Devices         : ${DEVICES}"
+ui_print "- Devices         : ${DEVICES:-Unknown}"
 sleep 0.2
-ui_print "- Manufacturer    : ${MANUFACTURER}"
-ui_print "***************************************"
-sleep 0.2
+ui_print "- Manufacturer    : ${MANUFACTURER:-Unknown}"
+print_line
 ui_print "- Trimming up Partitions"
-trim_partition &>/dev/null
-ui_print "- Delete trash and logs"
-delete_trash_logs &>/dev/null
-ui_print "- Final Installation Tweaks"
-install_script &>/dev/null
-sleep 0.5
+execute_task "trim_partition"
+ui_print "- Deleting Trash and Logs"
+execute_task "delete_trash_logs"
+ui_print "- Performing Memory Purge"
+execute_task "ram_cleanup"
+ui_print "- Adjustments Advanced settings"
+execute_task "install_tweak"
 ui_print "- Done"
 ui_print ""
+
+# closing or peak completion of the flash module
+wait && logcat -c && send_notification
