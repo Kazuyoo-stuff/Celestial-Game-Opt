9f58771bb5569d64038940c8b2fd457a76e51ae2
Kazuyoo_12 <dikinurrohmat42@gmail.com>
2025-04-18 18:41:19 +0700

    Add files via upload

--- source/C-Game-Opt-Service.sh
+++ source/C-Game-Opt-Service.sh
@@ -29,15 +29,8 @@ game_manager() {
 
 # Loop each package and set Game Mode and Overlay
     for package in $GAME_LIST; do
-        if cmd game mode performance "$package"; then
-            if cmd device_config put game_overlay "$package" mode=2,fps=120:mode=3,fps=60; then
-                ui_print "- Successfully set Game Mode for $package"
-            else
-                ui_print "- Failed to apply overlay settings for $package"
-            fi
-        else
-            ui_print "- Failed to set Game Mode for $package"
-        fi
+       cmd game mode performance "$package"
+       cmd device_config put game_overlay "$package" mode=2,fps=120:mode=3,fps=60
     done
 }
 
@@ -92,52 +85,54 @@ refresh_rate=$(dumpsys SurfaceFlinger | grep "refresh-rate" | awk '{print $3}' |
 frame_time=$(awk "BEGIN {printf \"%.0f\", (1 / $refresh_rate) * 1000000000}")
 
 # Calculate various offsets based on frame_time
-phazev1=$((frame_time / 8))
-phazev2=$((frame_time / 5))
-phazev3=$((frame_time / 3))
-phazev4=$((frame_time / 2))
-phazev5=$((frame_time * 2 / 3))
-phazev6=$((frame_time))
-phazev7=$((frame_time * 5 / 4))
+early_offset=$((frame_time / 3))
+late_offset=$((frame_time / 2))
+default_offset=$((frame_time))
+negative_offset=$((early_offset * -1))
+gl_duration=$((frame_time * 2 / 3))
+idle_timer=$((frame_time / 2))
+caching_timeout=$((frame_time * 2))
+sampling_duration=$((frame_time / 2))
+sampling_period=$((frame_time))
 
 # Apply SurfaceFlinger settings with optimal phazev 
-setprop debug.sf.earlyGl.app.duration "$phazev5"
-setprop debug.sf.earlyGl.sf.duration "$phazev5"
-setprop debug.sf.hwc.min.duration "$phazev5"
-setprop debug.sf.early.app.duration "$phazev5"
-setprop debug.sf.late.app.duration "$phazev6"
-setprop debug.sf.early.sf.duration "$phazev5"
-setprop debug.sf.late.sf.duration "$phazev6"
+setprop debug.sf.earlyGl.app.duration "$gl_duration"
+setprop debug.sf.earlyGl.sf.duration "$gl_duration"
+setprop debug.sf.hwc.min.duration "$gl_duration"
 
-setprop debug.sf.set_idle_timer_ms "$phazev4"
-setprop debug.sf.layer_caching_active_layer_timeout_ms "$phazev3"
+setprop debug.sf.early.app.duration "$gl_duration"
+setprop debug.sf.late.app.duration "$default_offset"
+setprop debug.sf.early.sf.duration "$gl_duration"
+setprop debug.sf.late.sf.duration "$default_offset"
 
-setprop debug.sf.high_fps_early_app_phase_offset_ns "-$phazev3"
-setprop debug.sf.high_fps_late_app_phase_offset_ns "$phazev2"
-setprop debug.sf.high_fps_early_sf_phase_offset_ns "-$phazev3"
-setprop debug.sf.high_fps_late_sf_phase_offset_ns "$phazev2"
-setprop debug.sf.high_fps_early_gl_app_phase_offset_ns "$phazev1"
-setprop debug.sf.high_fps_early_gl_phase_offset_ns "$phazev2"
-setprop debug.sf.high_fps_early_phase_offset_ns "$phazev2"
-setprop debug.sf.high_fps_late_app_phase_offset_ns "$phazev6"
-setprop debug.sf.high_fps_late_sf_phase_offset_ns "$phazev6"
+setprop debug.sf.set_idle_timer_ms "$idle_timer"
+setprop debug.sf.layer_caching_active_layer_timeout_ms "$caching_timeout"
 
-setprop debug.sf.vsync_phase_offset_ns "-$phazev2"
-setprop debug.sf.vsync_event_phase_offset_ns "-$phazev2"
+setprop debug.sf.high_fps_early_app_phase_offset_ns "$negative_offset"
+setprop debug.sf.high_fps_late_app_phase_offset_ns "$late_offset"
+setprop debug.sf.high_fps_early_sf_phase_offset_ns "$negative_offset"
+setprop debug.sf.high_fps_late_sf_phase_offset_ns "$late_offset"
 
-setprop debug.sf.region_sampling_duration_ns "$phazev4"
-setprop debug.sf.cached_set_render_duration_ns "$phazev4"
+setprop debug.sf.high_fps_early_gl_app_phase_offset_ns "$early_offset"
+setprop debug.sf.high_fps_early_gl_phase_offset_ns "$early_offset"
+setprop debug.sf.high_fps_early_phase_offset_ns "$early_offset"
+setprop debug.sf.high_fps_late_app_phase_offset_ns "$default_offset"
+setprop debug.sf.high_fps_late_sf_phase_offset_ns "$default_offset"
 
-setprop debug.sf.early_app_phase_offset_ns "$phazev2"
-setprop debug.sf.early_gl_app_phase_offset_ns "$phazev2"
+setprop debug.sf.vsync_phase_offset_ns "$negative_offset"
+setprop debug.sf.vsync_event_phase_offset_ns "$negative_offset"
 
-setprop debug.sf.early_gl_phase_offset_ns "$phazev4"
-setprop debug.sf.early_phase_offset_ns "$phazev4"
+setprop debug.sf.region_sampling_duration_ns "$sampling_duration"
+setprop debug.sf.cached_set_render_duration_ns "$sampling_duration"
 
-setprop debug.sf.region_sampling_timer_timeout_ns "$phazev7"
+setprop debug.sf.early_app_phase_offset_ns "$early_offset"
+setprop debug.sf.early_gl_app_phase_offset_ns "$early_offset"
+setprop debug.sf.early_gl_phase_offset_ns "$sampling_duration"
+setprop debug.sf.early_phase_offset_ns "$sampling_duration"
 
-setprop debug.sf.region_sampling_period_ns "$phazev6"
-setprop debug.sf.phase_offset_threshold_for_next_vsync_ns "$phazev6"
+setprop debug.sf.region_sampling_timer_timeout_ns "$default_offset"
+setprop debug.sf.region_sampling_period_ns "$default_offset"
+setprop debug.sf.phase_offset_threshold_for_next_vsync_ns "$default_offset"
 }
 
 final_optimization() {
@@ -190,7 +185,7 @@ final_optimization() {
   cmd thermalservice override-status 0
 
 # Run simpleperf for lighter logging
-  simpleperf --log fatal --log-to-android-buffer 6
+  simpleperf --log fatal --log-to-android-buffer 0
   
 # fstrim every reboot
   write global fstrim_mandatory_interval 1
@@ -203,12 +198,23 @@ final_optimization() {
 # Disable app standby ( battery usage may increase )
   write global app_standby_enabled 0
   
+# Set how fast the touch is and determine the time limit between taps.
+  write secure long_press_timeout 200
+  write secure multi_press_timeout 190
+  
 # device_config optimization from nrao [ https://github.com/iamlooper/NRAO ]
   cmd device_config put runtime_native_boot disable_lock_profiling true
   cmd device_config put runtime_native_boot iorap_readahead_enable true
   cmd device_config put activity_manager max_phantom_processes 2147483647
   cmd device_config put activity_manager max_cached_processes 256
   cmd device_config put activity_manager max_empty_time_millis 43200000
+  
+# Optimize Jank https://android.googlesource.com/platform/frameworks/base.git/+/refs/heads/main/core/java/com/android/internal/jank/InteractionJankMonitor.java
+  device_config put interaction_jank_monitor enabled false
+  device_config put interaction_jank_monitor trace_threshold_missed_frames 1
+  device_config put interaction_jank_monitor trace_threshold_frame_time_millis 24
+  device_config put interaction_jank_monitor debug_overlay_enabled false
+  device_config put interaction_jank_monitor sampling_interval 10
 }
   
 # ----------------- MAIN EXECUTION -----------------
